de6b227be8be50341e6ab82fb4374f5781ec9f31
mucalytic <mucalytic@hotmail.com>
2025-02-04 10:19:52 +0100

    Added thread safe naive singleton with lazy loading.

---
+++ Design.Patterns.Naive.Singleton.Lazy.Loading.Thread.Safe/Singleton.cs
@@ -0,0 +1,22 @@
+﻿namespace Design.Patterns.Naive.Singleton.Lazy.Loading.Thread.Safe;
+
+public sealed class Singleton
+{
+    private static Singleton? _instance;
+
+    private static readonly object Lock = new();
+    
+    private Singleton() { }
+
+    public static Singleton Instance
+    {
+        get // this implementation is called double-checked locking
+        {
+            if (_instance is not null) return _instance;
+            lock (Lock) // locking is expensive so only do it once
+            {
+                return _instance ??= new Singleton();
+            }
+        }
+    }
+}
