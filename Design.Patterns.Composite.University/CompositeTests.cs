bf63bd7df45b6574a7d4f9380651131406ab126b
mucalytic <mucalytic@hotmail.com>
2025-02-06 14:20:48 +0100

    Updated tests for Composite University project.

--- Design.Patterns.Composite.University/CompositeTests.cs
+++ Design.Patterns.Composite.University/CompositeTests.cs
@@ -8,7 +8,7 @@ namespace Design.Patterns.Composite.University;
 public class CompositeTests
 {
     [Fact]
-    public void AllLeafOperationsAreRun_WhenRootOperationIsRun()
+    public void DurationForBundle_ShouldReturnSumOfCourseDurations()
     {
         // arrange
         var helper = Substitute.For<ITestOutputHelper>();
@@ -20,10 +20,26 @@ public class CompositeTests
 
         // act
         var duration = root.GetDuration();
-        var price = root.GetPrice();
 
         // assert
         duration.Should().Be(TimeSpan.FromHours(7));
+    }
+
+    [Fact]
+    public void PriceForBundle_ShouldReturnSumOfCoursePrices_WithTwentyPercentDiscount()
+    {
+        // arrange
+        var helper = Substitute.For<ITestOutputHelper>();
+        var leafA = new Course("Getting Started: Clean Architecture", TimeSpan.FromHours(3), 100m);
+        var leafB = new Course("Deep Dive: Clean Architecture", TimeSpan.FromHours(4), 110m);
+        var root = new Bundle(helper, "Zero to Hero: Clean Architecture");
+        root.Add(leafA);
+        root.Add(leafB);
+
+        // act
+        var price = root.GetPrice();
+
+        // assert
         price.Should().Be(168m);
     }
 }
